49379364c16cce5bfd8b0555e2480f88815fdc23
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-18 14:48:04 -0500

    add adopt button

--- react-project-117/src/PetCard.js
+++ react-project-117/src/PetCard.js
@@ -5,6 +5,7 @@ function PetCard({
   name,
   image = "https://upload.wikimedia.org/wikipedia/en/5/5f/Original_Doge_meme.jpg",
   favSnacks,
+  isAdopted,
 }) {
   const snackLis = favSnacks.map((snack) => <li key={snack}>{snack}</li>);
 
@@ -12,7 +13,7 @@ function PetCard({
     <div className="card">
       <h2>{name}</h2>
       <img src={image} alt={name} />
-      <EmojiButton label="Adopt" />
+      {isAdopted ? null : <EmojiButton label="Adopt" />}
       <h3>Favorite Snacks</h3>
       <ul>{snackLis}</ul>
     </div>
