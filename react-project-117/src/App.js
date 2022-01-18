f34a52dd68257f493e8ae07551e9f4bcb176aae2
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-18 15:10:47 -0500

    place data in separate data file

--- react-project-117/src/App.js
+++ react-project-117/src/App.js
@@ -1,34 +1,21 @@
-import React from "react";
-import EmojiButton from "./EmojiButton";
+import React, { useState } from "react";
 import Header from "./Header";
 import PetCard from "./PetCard";
+import { pets as petsArray } from "./data";
 
-const pets = [
-  {
-    id: 1,
-    name: "Fezzik",
-    image:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-small-dog-breeds-1610571819.jpg?crop=1.00xw:1.00xh;0,0&resize=640:*",
-    favSnacks: ["Peanut Butter", "Sweet Potato Treats"],
-    isAdopted: false,
-  },
-  {
-    id: 2,
-    name: "Lucy",
-    image:
-      "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg",
-    favSnacks: ["DoggieRice", "Bacon"],
-    isAdopted: true,
-  },
-  {
-    id: 3,
-    name: "Blaise",
-    favSnacks: ["Cheetos", "Grass", "Beef"],
-    isAdopted: true,
-  },
-];
+/*
+
+Component Hierarchy
+
+  App
+  ├───Header
+  └───PetCard
+  
+*/
 
 function App() {
+  const [pets, setPets] = useState(petsArray)
+  
   const petCards = pets.map((petObj) => {
     return (
       <PetCard
@@ -43,7 +30,7 @@ function App() {
 
   return (
     <div>
-      <Header isLoggedIn={true} />
+      <Header />
       <main>{petCards}</main>
     </div>
   );
---
+++ react-project-117/src/data.js
@@ -0,0 +1,26 @@
+const pets = [
+  {
+    id: 1,
+    name: "Fezzik",
+    image:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/23124207/Old-English-Sheepdog-running-in-the-park.jpg",
+    favSnacks: ["Peanut Butter", "Soccer Balls"],
+    isAdopted: false,
+  },
+  {
+    id: 2,
+    name: "Lucy",
+    image:
+      "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg",
+    favSnacks: ["Cake", "Bacon"],
+    isAdopted: true,
+  },
+  {
+    id: 3,
+    name: "Blaise",
+    favSnacks: ["Pigeons", "Grass", "Chicken"],
+    isAdopted: true,
+  },
+];
+
+export { pets };
