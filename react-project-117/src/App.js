7280e58c8ba75211ee10d2913498cfe31b57bf75
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-18 14:41:09 -0500

    add isLoggied in function

--- react-project-117/src/App.js
+++ react-project-117/src/App.js
@@ -43,7 +43,7 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <Header isLoggedIn={true} />
       <main>{petCards}</main>
     </div>
   );
--- react-project-117/src/Header.js
+++ react-project-117/src/Header.js
@@ -4,13 +4,11 @@ import Login from "./Login";
 import Logout from "./Logout";
 
 function Header({ isLoggedIn }) {
-  console.log(isLoggedIn);
+  
   return (
     <header>
       <h1>PetLand</h1>
-      <nav>
-        <Login />
-      </nav>
+      <nav>{isLoggedIn ? <Logout /> : <Login />}</nav>
     </header>
   );
 }
