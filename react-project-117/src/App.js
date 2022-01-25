36323c527b1572f28f1a3b361fa57352f7d9ad29
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-24 19:11:11 -0500

    add nav bootstrap

--- react-project-117/src/App.js
+++ react-project-117/src/App.js
@@ -4,7 +4,7 @@ import PetCard from "./PetCard";
 import PetForm from "./PetForm";
 import { pets as petsArray } from "./data";
 import "./style.css";
-
+import {Navbar, Nav, Container} from 'react-bootstrap';
 
 function App() {
   const [pets, setPets] = useState([]);
@@ -33,6 +33,48 @@ function App() {
     console.log("in App", petObj);
     setPets([...pets, petObj]);
   }
+
+
+return (
+  <>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+  <br />
+  <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+</>
+
+export default App;
+
+
 
   return (
     <div>
